103364267d66263816f67f258bb2e1f55df0ed57
Heinrich Schewe <heinrich_schewe@trimble.com>
2023-03-27 19:01:37 +0200

    added polar projections

--- graphicsEarthCurvature/hedgehog_planar.py
+++ graphicsEarthCurvature/hedgehog_planar.py
@@ -4,7 +4,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import sys
 
-def vectors(df, stride, meridian):
+def vectors(df, stride, meridian, xmin, xmax, ymin, ymax):
     rx = []
     ry = []
     rs = []
@@ -19,6 +19,10 @@ def vectors(df, stride, meridian):
     for x,y, u,v,w, u1,v1,w1 in df:
         plot = True
         if y == -90 or y == 90:
+            plot = False
+        if x <xmin or x>xmax:
+            plot = False
+        if y<ymin or y>ymax:
             plot = False
         if xalt != x:
             xalt = x
@@ -87,10 +91,13 @@ def filterValues(xx, yy, sc):
         z.append(s[(k1,k2)])
     return x, y, z, s
 
+
+#################################### main ####################################
+
 if len(sys.argv) > 1:    
     droppedFile = sys.argv[1].strip().replace('\\', '/')
 else:
-    droppedFile = 'D:/Data/inpho/IS-8654/GK-45-9-H0_d10_vec.txt'
+    droppedFile = 'D:/Data/inpho/IS-8654/Lambert93-Korsika-H0_d10_vec.txt'
 
 #data
 f = open(droppedFile, "r")
@@ -99,13 +106,32 @@ meridian = float(f.readline())
 f.close()
 
 nd = np.genfromtxt(droppedFile, skip_header=2)
+fig = plt.figure(figsize=(13,10))
+ax = fig.add_subplot(211)
+axn = fig.add_subplot(223, projection='polar')
+axz = fig.add_subplot(224, projection='polar')
 
-# out = df.pivot_table(2, pd.cut(df[1], bins=17), pd.cut(df[0], bins=37), aggfunc='mean')
+axn.set_theta_zero_location('N')
+axn.set_theta_direction('clockwise')
+axn.set_rmin(-90)
+axn.set_rmax(-10)
+axn.set_rticks([-80,-60,-40,-20])
+axn.set_thetagrids((0,90,180,270), labels=('N', 'E','S','W'))
+axn.set_title("Nadir view (looking downward)")
 
-fig, ax = plt.subplots(figsize=(10,5))
+axz.set_theta_zero_location('S')
+axz.set_rmin(90)
+axz.set_rmax(10)
+axz.set_rticks([80,60,40,20])
+axz.set_thetagrids((0,90,180,270), labels=('N', 'E','S','W'))
+axz.set_title("Zenith view (looking upward)")
+
 
 sc = scales(nd)
-x, y, u, v, max = vectors(nd, 2, meridian)
+
+####################
+
+x, y, u, v, max = vectors(nd, 2, meridian, 0, 360, -80, 80)
 
 qu1 = ax.quiver(x, y, u, v, angles='xy', width = 0.002)
 qu1._init()
@@ -135,7 +161,7 @@ zz = np.array(z).reshape(37,17)
 
 im = ax.pcolormesh(xx, yy, zz, alpha=0.4, cmap=cmap, vmin=0.998, vmax = 1.002)
 
-# show scale at 10, 90, 170 deg vertical angle
+# show scale at -80, 0, 80 deg vertical angle
 for v in [-80,0,80]:
     val = s[(0,v)]
     ax.text(0, v, "{:.5f}".format(val))
@@ -144,6 +170,51 @@ fig.colorbar(im, ax=ax)
 ax.set_ylabel('vertical angle')
 ax.set_xlabel('azimuth (0->N, 90->E)')
 ax.set_title(droppedFile)
+
+#############
+
+xg = np.linspace(0, 360, 37)
+yg = np.linspace(-90, 0, 10)
+x, y, z, s = filterValues(xg, yg, sc)
+xx = np.array(x).reshape(37,10)
+yy = np.array(y).reshape(37,10)
+zz = np.array(z).reshape(37,10)
+
+rad = np.linspace(-90, 0, 10)
+azm = np.linspace(0, 2 * np.pi, 37)
+r, th = np.meshgrid(rad, azm)
+
+im = axn.pcolormesh(th, r, zz, alpha=0.4, cmap=cmap, vmin = 0.998, vmax = 1.002)
+# show scale at 0, -90
+for v in [-90,0]:
+    val = s[(0,v)]
+    axn.text(0, v, "{:.5f}".format(val))
+
+x, y, u, v, max = vectors(nd, 0, meridian, 0, 360, -90, 0)
+xv = []
+for a in x:
+    xv.append(a/180*np.pi)
+
+qu1 = axn.quiver(xv, y, u, v, angles='xy', width = 0.003)
+qu1._init()
+qu1.scale = scale
+
+xm = [45, 135, 225, 315]
+ym = [0, 0, 0, 0]
+um, vm = [], []
+for i, a in enumerate(xm):
+    xm[i] = xm[i]/180*np.pi
+for x, y in zip(xm, ym):
+    um.append(meridian*180/np.pi)
+    vm.append(0)
+
+maxi = (1000*meridian*180/np.pi)
+maxf = maxi/1000
+qu = axn.quiver(xm, ym, um, np.zeros(shape=len(um)), angles='xy', width = 0.004, color='r', pivot='mid')
+qu._init()
+axn.text(xm[0], ym[0], s="meridian convergence: {:.3f}".format(maxf), color="r")
+
+
 
 plt.tight_layout()
 plt.show()
